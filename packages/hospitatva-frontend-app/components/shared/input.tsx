aed01245c117a52a3e30116349ca913d3b9bd453
jagnani73 <yashjagnani73@gmail.com>
2022-03-17 07:34:10 +0530

    feat(fe/app/home: add input

---
+++ packages/hospitatva-frontend-app/components/shared/input.tsx
@@ -0,0 +1,117 @@
+import { Field } from "formik";
+
+import { CustomFieldTypes } from "../../utils/interfaces/shared";
+
+const Input = (props: CustomFieldTypes) => {
+  switch (props.type) {
+    case "text":
+    case "email":
+    case "date":
+    case "number":
+    case "password":
+    case "textarea": {
+      return (
+        <div className={props.classNames?.wrapper ?? ""}>
+          {props.label && (
+            <label
+              htmlFor={props.id}
+              className={`${props.classNames?.label ?? ""}`}
+            >
+              {props.label}
+            </label>
+          )}
+          {props.type === "textarea" ? (
+            <Field
+              {...props}
+              component="textarea"
+              className={`${props.classNames?.input ?? ""}`}
+            />
+          ) : (
+            <Field {...props} className={`${props.classNames?.input ?? ""}`} />
+          )}
+
+          {props.description && (
+            <p className={props.classNames?.description ?? ""}>
+              {props.description}
+            </p>
+          )}
+        </div>
+      );
+    }
+
+    case "select": {
+      return (
+        <div className={props.classNames?.wrapper}>
+          {props.label && (
+            <label
+              htmlFor={props.id}
+              className={`${props.classNames?.label ?? ""}`}
+            >
+              {props.label}
+            </label>
+          )}
+
+          <Field
+            {...props}
+            as="select"
+            className={props.classNames?.input ?? ""}
+          >
+            <option className={props.classNames?.option ?? ""} value="">
+              {props.placeholder ?? "Select"}
+            </option>
+            {props.choices?.map((choice) => (
+              <option
+                key={choice.value.trim()}
+                className={props.classNames?.option ?? ""}
+                value={choice.value}
+              >
+                {choice.text}
+              </option>
+            ))}
+          </Field>
+
+          {props.description && (
+            <p className={props.classNames?.description ?? ""}>
+              {props.description}
+            </p>
+          )}
+        </div>
+      );
+    }
+
+    case "radio":
+    case "checkbox": {
+      return (
+        <div className={props.classNames?.wrapper ?? ""} role="group">
+          {props.choices.map((choice) => (
+            <label
+              htmlFor={`${props.name}.${choice.value}`}
+              className={props.classNames?.label ?? ""}
+            >
+              <Field
+                {...props}
+                id={`${props.name}.${choice.value}`}
+                placeholder={props.placeholder ?? ""}
+                value={choice.value}
+                className={props.classNames?.option ?? ""}
+              />
+              <div className="inline-block">{choice.text}</div>
+            </label>
+          ))}
+
+          {props.description && (
+            <p className={props.classNames?.description ?? ""}>
+              {props.description}
+            </p>
+          )}
+        </div>
+      );
+    }
+
+    default: {
+      return <></>;
+    }
+  }
+};
+
+export default Input;
--- packages/hospitatva-frontend-app/utils/interfaces/shared.tsx
+++ packages/hospitatva-frontend-app/utils/interfaces/shared.tsx
@@ -2,3 +2,62 @@ export interface HeaderProps {
   type: "primary" | "secondary";
   children: React.ReactNode;
 }
+
+export interface Validation {
+  isError?: boolean;
+  validationType?: string;
+  validations?: {
+    type: string;
+    params?: (string | number | RegExp | any)[];
+  }[];
+}
+
+interface CustomFieldProps extends Validation {
+  name: string;
+  id: string;
+  placeholder?: string;
+  type:
+    | "text"
+    | "textarea"
+    | "date"
+    | "select"
+    | "email"
+    | "number"
+    | "password"
+    | "checkbox"
+    | "radio";
+  description?: string;
+  label?: string;
+  classNames?: Partial<{
+    wrapper: string;
+    input: string;
+    description: string;
+    label: string;
+    option: string;
+  }>;
+}
+
+export interface CustomInputProps extends CustomFieldProps {
+  type: "text" | "date" | "email" | "password" | "number";
+}
+
+export interface CustomTextareaProps extends CustomFieldProps {
+  type: "textarea";
+}
+
+export interface CustomSelectProps extends CustomFieldProps {
+  type: "select";
+  choices: { value: string; text: string }[];
+  placeholder?: string;
+}
+
+export interface CustomRadioBoxProps extends CustomFieldProps {
+  type: "radio" | "checkbox";
+  choices: { value: string; text: string }[];
+}
+
+export type CustomFieldTypes =
+  | CustomInputProps
+  | CustomTextareaProps
+  | CustomSelectProps
+  | CustomRadioBoxProps;
